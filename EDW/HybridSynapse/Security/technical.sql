9d2fcff1e672b53a3bf309f0d626f7ce97fe512b
andrewlimh-suncor <alimheloe@suncor.com>
2022-01-16 21:40:53 -0800

    add

--- EDW/HybridSynapse/Security/technical.sql
+++ EDW/HybridSynapse/Security/technical.sql
@@ -1,3 +1,5 @@
 ﻿CREATE SCHEMA [technical]
     AUTHORIZATION [dbo];
 
+
+
--- EDW/HybridSynapse/technical/Tables/person.sql
+++ EDW/HybridSynapse/technical/Tables/person.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [technical].[person] (
-    [id]   INT           NULL,
-    [name] VARCHAR (100) NULL
+    [id]         INT           NULL,
+    [first_name] VARCHAR (100) NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
+
+
 
