19b70dc64ecb3df995cb736676e4c7a5fd343d2f
andrewlimh-suncor <alimheloe@suncor.com>
2022-01-16 21:54:06 -0800

    add

--- EDW/HybridSynapse/technical/Tables/person.sql
+++ EDW/HybridSynapse/technical/Tables/person.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [technical].[person] (
     [id]         INT           NULL,
-    name VARCHAR (100) NULL
+    first_name VARCHAR (100) NULL
 )
 WITH (CLUSTERED COLUMNSTORE INDEX, DISTRIBUTION = ROUND_ROBIN);
 
